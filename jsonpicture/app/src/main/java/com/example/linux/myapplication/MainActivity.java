19b120514ea227094725ece1dabc31708c4c7250
jutturong <jutturongcharoenrit@gmail.com>
2016-11-09 23:03:30 +0700

    add autocomplete 9-10-59

--- jsonpicture/app/src/main/java/com/example/linux/myapplication/MainActivity.java
+++ jsonpicture/app/src/main/java/com/example/linux/myapplication/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.linux.myapplication;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -27,10 +34,16 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    public  String  url="http://192.168.2.112/dental/index.php/json/tb_herb";
+    public  String  url="http://192.168.2.120/dental/index.php/json/tb_herb";
+
+    private static final String[] COUNTRIES = new String[] {
+            "Belgium", "France", "Italy", "Germany", "Spain", "Thailand", "Taiwan"
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,9 +56,24 @@ public class MainActivity extends AppCompatActivity {
             StrictMode.setThreadPolicy(policy);
         }
 
+
+
+        // autoCompleteTextView1
+        final AutoCompleteTextView autocomplete1 = (AutoCompleteTextView)findViewById(R.id.autoCompleteTextView1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+
+        autocomplete1.setAdapter(adapter);
+
+
+
+
         final LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
 
         final ListView listView1=(ListView) findViewById(R.id.listView1);
+
+
 
         final Button btn1=(Button) findViewById(R.id.btn1);
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -77,6 +105,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
                        // Toast.makeText(MainActivity.this,String.valueOf(  c.getString("name") ),Toast.LENGTH_SHORT).show();
 
                     }
@@ -87,6 +116,50 @@ public class MainActivity extends AppCompatActivity {
                             new String[] {"name", "name_sci", }, new int[] {R.id.Col_name, R.id.Col_name_sci, });
                     listView1.setAdapter(sAdap);
 
+
+
+
+
+                    final AlertDialog.Builder viewDetail = new AlertDialog.Builder(MainActivity.this);
+
+                    listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        public void onItemClick(AdapterView<?> myAdapter, View myView,
+                                                int position, long mylng) {
+
+                            String sname = MyArrList.get(position).get("name")
+                                    .toString();
+
+                            String name_sci = MyArrList.get(position).get("name_sci")
+                                    .toString();
+
+                            String name_local = MyArrList.get(position).get("name_local")
+                                    .toString();
+
+                            // Toast.makeText(MainActivity.this,String.valueOf(  sname ),Toast.LENGTH_SHORT).show();
+
+                            viewDetail.setIcon(android.R.drawable.btn_star_big_on);
+                          //  viewDetail.setTitle("แสดงรายละเอียดย่อย");
+
+                            viewDetail.setMessage(
+                                    "ชื่อวิทยาศาสตร์ : " + name_sci + "\n"
+                                    + "ชื่อสามัญ : " + name_local + "\n"
+                                                 );
+
+                            viewDetail.setPositiveButton("OK",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog,
+                                                            int which) {
+                                            // TODO Auto-generated method stub
+                                            dialog.dismiss();
+                                        }
+                                    });
+
+                            viewDetail.show();
+
+
+                        }
+
+                    });
 
 
 
@@ -106,6 +179,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
 
 
     public String getJSONUrl(String url) {
